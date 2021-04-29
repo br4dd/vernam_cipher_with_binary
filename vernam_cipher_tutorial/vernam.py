974abb59d0897f6f00bc19feb2f0bf0140568cda
br4dd <earljohn321123@gmail.com>
2021-04-29 21:52:24 +0800

    revised code with methods

--- vernam_cipher_tutorial/vernam.py
+++ vernam_cipher_tutorial/vernam.py
@@ -9,6 +9,19 @@ def VernamCipherFunction(text, key):
                       
 encryption_key = '';
 
+def encrypted_text(encryption):
+      print('\nENCRYPTED TEXT: '+encryption)
+def encrypted_text_binary(encryption_key_binary):
+      print('\nENCRYPTED TEXT IN BINARY: '+encryption_key_binary+'\n')
+def plaintext_binary_output():
+      print('%s %s= %s'% ( plaintext, ' '*(len(encryption_key)-len(plaintext)) , plaintext_binary ) )
+def encryption_binary_output():
+      print('%s = %s'% ( encryption_key, encryption_key_binary ) )
+def dashes():
+      print('%s %s' % ( ' '*(len(encryption_key) + 2) ,'⎻'*len(encryption_key_binary)) )
+def encrypted_text_output():
+      print('%s %s' % ( ' '*(len(encryption_key) + 2) ,result_binary ) )
+
 while True:
       plaintext = input("\nEnter Text To Encrypt:\t");
       encryption_key = input("\nEnter Encryption Key:\t");
@@ -17,28 +30,35 @@ while True:
         
         print('\nENCRYPTION\n')
 
-        #FOR CONVERTING VARIABLES TO BINARY
+      # FOR CONVERTING VARIABLES TO BINARY
         plaintext_binary = ' '.join(format(ord(i), '08b') for i in plaintext)
         encryption_key_binary = ' '.join(format(ord(i), '08b') for i in encryption_key)
         result_binary = ' '.join(format(ord(i), '08b') for i in encryption)
-        
-        print('%s %s= %s'% ( plaintext, ' '*(len(encryption_key)-len(plaintext)) , plaintext_binary ) )
-        print('%s = %s'% ( encryption_key, encryption_key_binary ) )
-        print('%s %s' % ( ' '*(len(encryption_key) + 2) ,'⎻'*len(encryption_key_binary)) )
-        # XOR OPERATION
-        print('%s %s' % ( ' '*(len(encryption_key) + 2) ,result_binary ) )
-        print('\nENCRYPTED TEXT: '+encryption)
+      # ENCRYPTION
+        plaintext_binary_output()
+        encryption_binary_output()
+        dashes()
+      # XOR OPERATION
+        encrypted_text_output()
+
+        encrypted_text(encryption)
+        encrypted_text_binary(encryption_key_binary)
+
         print('NOTE: SOME OF THE ENCRYPTED TEXT ARE NOT CONVERTABLE TO NORMAL CHARACTER SO THE OUTPUT WILL BE BLANK')
+
         decryption = VernamCipherFunction(encryption, encryption_key);
         print("\nDecrypted Vernam Cipher Text:\t" + decryption+"\n");
-        #DECRYPTION
-        print('%s %s' % ( ' '*(len(encryption_key) + 2) ,result_binary ) )
-        print('%s = %s'% ( encryption_key, encryption_key_binary ) )
-        print('%s %s' % ( ' '*(len(encryption_key) + 2) ,'⎻'*len(encryption_key_binary)) )
-        print('%s %s= %s'% ( plaintext, ' '*(len(encryption_key)-len(plaintext)) , plaintext_binary ) )
-
+      # DECRYPTION
+        encrypted_text_output()
+        encryption_binary_output()
+        dashes()
+      # XOR OPERATION
+        plaintext_binary_output()
+        
       else:
           print('\nENCRYPTION KEY MUST BE EQUAL OR MORE THAN THE LENGTH OF PLAINTEXT')
+
+
 
 # chr(97) => 'a'
 # ord('a') => 97
